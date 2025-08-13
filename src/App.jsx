44f5c34e522e2537d59742d6d9d9181ed4002d1f
hadi osama <hadiosama622@gmail.com>
2025-08-13 16:48:11 +0300

    to change branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,24 @@ import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
 import SignUp from './components/SignUp/SignUp'
 import SignIn from './components/SignIn/SignIn'
-import MovieList from './components/MovieList/MovieList.jsx'
-import { Route, Routes, Navigate } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import * as authService from './services/authService.js'
-import { useEffect, useState } from 'react'
-
+import * as movieService from './services/movieService.js'
+import { useState } from 'react'
 
 const App = () => {
+  const navigate = useNavigate()
 
   const initialState = authService.getUser()
-
   const [user, setUser] = useState(initialState)
   const [movies, setMovies] = useState([])
-
-  useEffect(() => {
-    const fetchAllMovies = async () => {
-      const moviesData = await movieService.index()
-      console.log('Fetched movies:', moviesData)
-      setMovies(moviesData)
-    }
-  fetchAllMovies()
-  }, [])
 
   const handleSignUp = async (formData) => {
     try {
       const res = await authService.signUp(formData)
       setUser(res)
-      // return success
       return { success: true }
-    } catch(err){
-      // return failure flag (then signup form can display message)
-      // add message?
+    } catch (err) {
       return { success: false, message: err.message }
     }
   }
@@ -48,35 +35,58 @@ const App = () => {
       const res = await authService.signIn(formData)
       setUser(res)
       return { success: true }
-    } catch(err) {
+    } catch (err) {
       return { success: false, message: err.message }
     }
   }
 
-  return (
-    <div className="app-container">
-      <NavBar user={user} handleSignOut={handleSignOut} />
-      <div className="main-content">
-        <Routes>
-            <Route path='/' element={<h1>Welcome to Tickets Website!</h1>} />
-            <Route 
-              path='/sign-up' 
-              element={user ? <Navigate to="/" /> : <SignUp handleSignUp={handleSignUp} user={user} />} 
-            />
-            <Route 
-              path='/sign-in' 
-              element={user ? <Navigate to="/" /> : <SignIn handleSignIn={handleSignIn} user={user} />} 
-            />
-            <Route path="/movies" element={<MovieList movies={movies} />} />
-            <Route path='*' element={<h1>404 - Page Not Found</h1>} />
-        </Routes>
-      </div>
-      <Footer />
-    </div>
+  const handleAddMovie = async (formData) => {
+    try {
+      const newMovie = await movieService.create(formData)
+      setMovies([newMovie, ...movies])
+      navigate('/movies')
+    } catch (err) {
+      console.error('Failed to create movie:', err)
+    }
+  }
 
+  const handleDeleteMovie = async (movieId) => {
+    try {
+      await movieService.deleteMovie(movieId)
+      setMovies(movies.filter((movie) => movie._id !== movieId))
+      navigate('/movies')
+    } catch (err) {
+      console.error('Failed to delete movie:', err)
+    }
+  }
+
+  const handleUpdateMovie = async (formData, movieId) => {
+    const updatedMovie = await movieService.update(formData, movieId)
+    navigate(`/movies/${movieId}`)
+  }
+
+  return (
+    <>
+      <NavBar user={user} handleSignOut={handleSignOut} />
+      <Routes>
+        <Route path="/" element={<h1>Hello World!</h1>} />
+        {!user && (
+          <>
+            <Route
+              path="/sign-up"
+              element={<SignUp handleSignUp={handleSignUp} user={user} />}
+            />
+            <Route
+              path="/sign-in"
+              element={<SignIn handleSignIn={handleSignIn} user={user} />}
+            />
+          </>
+        )}
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
 export default App
-
-
