d98c4245f44c325dbd48ee6f0460acd978733fad
Ali Alashawal <aliashwal03@gmail.com>
2025-08-13 22:52:27 +0300

    Resolve App.jsx merge conflict with stashed changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,22 @@ import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import SignUp from './components/SignUp/SignUp'
 import SignIn from './components/SignIn/SignIn'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import * as authService from './services/authService.js'
 import { useState } from 'react'
+import AddMovie from './pages/AddMovie'
+import Footer from './components/Footer/Footer' // make sure Footer exists
 
 const App = () => {
-
   const initialState = authService.getUser()
-
   const [user, setUser] = useState(initialState)
 
   const handleSignUp = async (formData) => {
     try {
       const res = await authService.signUp(formData)
       setUser(res)
-      // return success
       return { success: true }
     } catch(err){
-      // return failure flag (then signup form can display message)
-      // add message?
       return { success: false, message: err.message }
     }
   }
@@ -38,36 +35,24 @@ const App = () => {
   return (
     <>
       <NavBar user={user} handleSignOut={handleSignOut} />
-      <Routes>
-          <Route path='/' element={<h1>Hello world!</h1>} />
-          <Route path='/sign-up' element={<SignUp handleSignUp={handleSignUp} user={user} />} />
-          <Route path='/sign-in' element={<SignIn handleSignIn={handleSignIn} user={user} />} />
-          <Route path='*' element={<h1>404</h1>} />
-    </Routes>
+      <div className="main-content">
+        <Routes>
+          <Route path='/' element={<h1>Welcome to Tickets Website!</h1>} />
+          <Route 
+            path='/sign-up' 
+            element={user ? <Navigate to="/" /> : <SignUp handleSignUp={handleSignUp} user={user} />} 
+          />
+          <Route 
+            path='/sign-in' 
+            element={user ? <Navigate to="/" /> : <SignIn handleSignIn={handleSignIn} user={user} />} 
+          />
+          <Route path='/add-movie' element={<AddMovie />} />
+          <Route path='*' element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
+      </div>
+      <Footer />
     </>
-
-    // <>
-    //   <NavBar user={user} handleSignOut={handleSignOut} />
-    //   <Routes>
-    //     <Route path="/" element={<h1>Hello World!</h1>} />
-    //     {!user && (
-    //       <>
-    //         <Route
-    //           path="/sign-up"
-    //           element={<SignUp handleSignUp={handleSignUp} />}
-    //         />
-    //         <Route
-    //           path="/sign-in"
-    //           element={<SignIn handleSignIn={handleSignIn} />}
-    //         />
-    //       </>
-    //     )}
-    //     <Route path="*" element={<h1>404</h1>} />
-    //   </Routes>
-    // </>
   )
 }
 
 export default App
-
-
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,47 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = (props) => {
-
-
-
   return (
-    <nav>
-      {props.user ? (
-        <ul>
-          <li>Welcome {props.user.username}</li>
-          <li><Link to="/"> Home </Link></li>
-          <li><Link to='/' onClick={props.handleSignOut}>Sign Out</Link></li>
-        </ul>
-        ) : (
-          <ul>
-            <li><Link to="/sign-up">Sign Up</Link></li>
-            <li><Link to="/sign-in">Sign In</Link></li>
-          </ul>
-          ) }
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-brand">
+          Tickets
+        </Link>
+
+        <div className="navbar-menu">
+          <Link to="/" className="navbar-item">Home</Link>
+
+          {props.user ? (
+            <>
+              <span className="navbar-welcome">
+                Welcome, {props.user.username}!
+              </span>
+
+              {/* Show Add Movie button if user is admin */}
+              {props.user.isAdmin && (
+                <Link to="/add-movie" className="navbar-item navbar-admin">
+                  Add Movie
+                </Link>
+              )}
+
+              <button 
+                onClick={props.handleSignOut} 
+                className="navbar-item navbar-button"
+              >
+                Sign Out
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/sign-in" className="navbar-item">Sign In</Link>
+              <Link to="/sign-up" className="navbar-item navbar-signup">Sign Up</Link>
+            </>
+          )}
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar 
+export default NavBar;
